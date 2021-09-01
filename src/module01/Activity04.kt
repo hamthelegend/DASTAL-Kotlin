e9cbb401eae232531e1878ad8f9eee2f8439997c
hamthelegend <hamuelxd@gmail.com>
2021-09-01 21:21:24 +0800

    Finished Activity04.kt

--- src/module01/Activity04.kt
+++ src/module01/Activity04.kt
@@ -10,5 +10,19 @@ fun main() {
     val array2Name = "array2"
     val array2 = getArrayFromUser(array2Name)
     println()
-    (array1 + array2).sortedArray().print("mergedArray")
+
+    val array = (array1 + array2).sortedArray()
+    print("Enter the number to be deleted: ")
+    val numberToDelete = readLine()!!.toInt()
+    val position = array.indexOf(numberToDelete)
+    if (position == -1) {
+        println("$numberToDelete is not in the list.")
+    } else {
+        for (i in position until array.lastIndex) {
+            array[i] = array[i + 1]
+        }
+    }
+    println()
+    println("The merged array after the deletion: ")
+    array.print(range = 0 until array.lastIndex)
 }
