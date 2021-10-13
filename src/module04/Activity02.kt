7157ee89ca6925280939e2c1075e1b2088e246d4
hamthelegend <hamuelxd@gmail.com>
2021-10-13 19:15:11 +0800

    Added module04/Activity02.kt instructions

--- src/module04/Activity02.kt
+++ src/module04/Activity02.kt
@@ -1,5 +1,12 @@
 package module04
 
+/*
+To run this program in command line, run these two commands:
+kotlinc -include-runtime -d mod04-activity02.jar src/module04/Activity02.kt src/module04/Expression.kt src/extensions/stack/Stack.kt
+java -jar mod04-activity02.jar
+Note: make sure you have the latest Kotlin compiler and JRE installed
+*/
+
 fun main() {
     println("Separate individual integers with a space. ")
     println("Enter /exit to quit the program. ")
